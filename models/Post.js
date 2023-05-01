68af2ff44051bd2f098cb067571363e191253a83
Vraj Shah <vrajshah8@gmail.com>
2023-05-01 17:25:33 -0400

    Git ignore test

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,27 @@
 import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        location: String,
+        description: String,
+        picturePath: String,
+        userPicturePath: String,
+        likes:{
+            type: Map,
+            of: Boolean,
+        }
+    }
+)
