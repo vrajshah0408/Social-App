e2dd1b83d5932ab3119978ecb65d5869ebd16c2d
Vraj Shah <vrajshah8@gmail.com>
2023-05-04 18:59:00 -0400

    Commit message

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import { Home } from '@mui/icons-material';
+import{ BrowserRouter, Navigate, Routes, Route} from 'react-router-dom'
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from 'theme.js';
+function App() {
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo (() => createTheme(themeSettings(mode)), [mode]);
+
+  return <div className="App">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path='/' element={<LoginPage/>} />
+        <Route path='/home' element={<HomePage/>} />
+        <Route path='/profile/:userId' element={<ProfilePage/>} />
+      </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  </div>
+}    
+
+
+export default App;
---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+    return <div>loginPage</div>
+};
+
+export default LoginPage;
