c1694a7727ca65d4484d5fde7f91f51274af37ce
Vraj Shah <vrajshah8@gmail.com>
2023-05-07 01:21:18 -0400

    user widgets

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -1,10 +1,12 @@
+/** @format */
+
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-const WidgetWrapper = styled(Box)(({theme}) => ({
-    padding: "1.5rem 1.5rem 0.75rem 1.5 rem",
-    backgroundColor: theme.palette.background.alt,
-    backgroundRadius: "0.75rem"
-}))
+const WidgetWrapper = styled(Box)(({ theme }) => ({
+  padding: "1.5rem 1.5rem 0.75rem 1.5 rem",
+  backgroundColor: theme.palette.background.alt,
+  backgroundRadius: "0.75rem",
+}));
 
 export default WidgetWrapper;
--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -1,8 +1,10 @@
-import { 
-ManageAccountsOutlined,
-EditOutlined, 
-LocationOnOutlined, 
-WorkOutlineOutlined
+/** @format */
+
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
 } from "@mui/icons-material";
 import { Box, Typography, Divider, useTheme } from "@mui/material";
 import UserImage from "components/UserImage";
@@ -12,72 +14,84 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const UserWidget = ({userId, picturePath}) =>{
-    const [user, setUser] = useState(null);
-    const {palette} = useTheme();
-    const navigate = useNavigate();
-    const token = useSelector((state) => state.token);
-    const dark = palette.neutral.dark;
-    const medium = palette.neutral.medium;
-    const main = palette.neutral.main;
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.token);
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
 
-    const getUser = async () => {
-        const response = await fetch(`http://localhost:3001/users/${userid}`, {
-            method: "GET",
-            headers: {Authorization: `Bearer ${token}`},
-            });
-            const data = await response.json;
-            setUser(data);
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userid}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json;
+    setUser(data);
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+  id(!user);
+  {
+    return null;
+  }
 
-    };
-    useEffect(() =>{
-        getUser();
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
 
-    }, [])
-    id (!user)
-    {
-        return null;
-    }
-
-    const {
-        firstName,
-        lastName,
-        location,
-        occupation,
-        viewedProfile,
-        impressions,
-        friends,
-    } = user;
-
-    return(
-        <WidgetWrapper>
-            {/*First Row */}
-            <FlexBetween
-            gap="0.5rem"
-            pb="1.1rem"
-            onClick={() => navigate(`/profile/${userId}`)}
+  return (
+    <WidgetWrapper>
+      {/*First Row */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap={"1rem"}>
+          <UserImage image={picturePath} />
+          <Box>
+            <Typography
+              varient="h4"
+              color={dark}
+              fontWeight={"500"}
+              sx={{
+                "&hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
             >
-                <FlexBetween gap={"1rem"}>
-                    <UserImage image={picturePath} />
-                    <Box>
-                        <Typography
-                        varient="h4"
-                        color={dark}
-                        fontWeight={"500"}
-                        sx={{
-                            "&hover":{
-                                color: palette.primary.light,
-                                cursor: "pointer",
-                            },
-                        }}
-                        >
-                            {firstName} {lastName}
-                        </Typography>
-                        <Typography color={medium}>{friends.length} Friends</Typography>
-                    </Box>
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length} Friends</Typography>
+          </Box>
+          <ManageAccountsOutlined />
+        </FlexBetween>
 
-                </FlexBetween>
-            </FlexBetween>
-        </WidgetWrapper>
-    )
-}
+        <Divider />
+        {/*Second Row */}
+        <Box p="1rem 0">
+          <Box display={"flex"} alignItems={"center"} gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{location}</Typography>
+          </Box>
+          <Box display={"flex"} alignItems={"center"} gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{occupation}</Typography>
+          </Box>
+          {/*Third Row */}
+        </Box>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
+};
