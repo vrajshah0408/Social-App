1c7cb8d8fac3a7cdc15ca5c46eb7ffd5bf600ac9
Vraj Shah <vrajshah8@gmail.com>
2023-05-10 22:46:40 -0400

    mypostwidget

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -19,3 +19,43 @@ import {
   IconButton,
   InputBase,
 } from "@mui/material";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost, setPosts } from "state";
+
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { palette } = useTheme();
+  const { _id } = useSelector((state) => state.use);
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("width-min: 1000px");
+  const meduiumMain = palette.neutral.meduiumMain;
+  const medium = palette.neutral.medium;
+};
+
+const handlePost = async () => {
+  const formData = new FormData();
+  formData.append("userId", _id);
+  formData.append("description", post);
+  if (image) {
+    formData.append("picture", image);
+    formData.append("puctirePath", image.name);
+  }
+
+  const response = await fetch(`https://localhost:3001/posts`, {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+    body: formData,
+  });
+  const posts = await response.json();
+  dispatch(setPost({ posts }));
+  setImage(null);
+  setPost("");
+};
