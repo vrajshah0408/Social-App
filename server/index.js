36d2bde4fabaca0b253e967470fa4deeb0812e53
Vraj Shah <vrajshah8@gmail.com>
2023-05-24 00:07:45 -0400

    vercel final

--- server/index.js
+++ server/index.js
@@ -30,6 +30,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+//test
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
